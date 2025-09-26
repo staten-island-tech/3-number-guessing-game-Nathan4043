0d4445dd9bf490242157b56b81c31879b6958536
Nathan4043 <nathanr88@nycstudents.net>
2025-09-26 10:22:18 -0400

    Create Guess.py

---
+++ Guess.py
@@ -0,0 +1,9 @@
+import random 
+random_integer = random.randint(1, 10)
+intguess = int(input)
+print(random_integer)
+while not  random_integer = intguess :
+    print("Guess a number")
+if int(input) is intguess:
+    print("correct")
+
