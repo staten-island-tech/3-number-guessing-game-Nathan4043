749cab92cc8d02807e5493f5769fd70ff0c349b0
Nathan4043 <nathanr88@nycstudents.net>
2025-09-30 10:13:08 -0400

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,13 +1,48 @@
+# import random 
+# random_integer = random.randint(1, 10)
+# intguess = input
+# while not  random_integer == intguess:
+#     print("Guess a number")
+#     input(int)
+# if input(int) == intguess:
+#     print("correct")
+#     print(random_integer)
+# else: 
+#     print("wrong") 
+
+# import random 
+# random_integer = random.randint(1, 10)
+# while True:
+#     intguess=int(input("Guess a number 1-10"))
+#     if int(input) == intguess:
+#         print("correct")
+#         print(random_integer)
+#     else: 
+#         print("wrong")
+
+# import random 
+# random_integer = random.randint(1, 10)
+# while True:
+#     intguess=int(input("Guess a number 1-10:"))
+#     if  random_integer == intguess:
+#         print("correct")
+#         print(random_integer)
+#         break
+#     else: 
+#         print("wrong")
+
 import random 
 random_integer = random.randint(1, 10)
-intguess = input
-while not  random_integer == intguess:
-    print("Guess a number")
-    input(int)
-if input(int) == intguess:
-    print("correct")
-    print(random_integer)
-else: 
-    print("wrong")
-    
+while True:
+    intguess=int(input("Guess a number 1-10:"))
+    if  random_integer == intguess:
+        print("correct")
+        print(random_integer)
+        break
+    else: 
+        print("wrong")
+
+x=0    
+Guess=int(input(""))
+
 
