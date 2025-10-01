c0b757e1849b8d263b8938a0c68d43c9a2a98819
Nathan4043 <nathanr88@nycstudents.net>
2025-10-01 10:21:43 -0400

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,3 +1,5 @@
+
+#
 # import random 
 # random_integer = random.randint(1, 10)
 # intguess = input
@@ -31,18 +33,52 @@
 #     else: 
 #         print("wrong")
 
+# import random 
+# random_integer = random.randint(1, 10)
+# while True:
+#     intguess=int(input("Guess a number 1-10:"))
+#     if  random_integer == intguess:
+#         print("correct")
+#         print(random_integer)
+#         break
+#     else: 
+#         print("wrong")
+
+# import random 
+# random_integer = random.randint(1, 10)
+# Guess=0
+# while True:
+#     Guess=int(input("Guess Number"))
+#     if random_integer > Guess:
+#         print("is less than")
+#     if random_integer < Guess:
+#         print("is greater than")
+#     if random_integer == Guess:
+#         print("Correct!")
+#         break
+
 import random 
 random_integer = random.randint(1, 10)
+Guess=0
+history = []
 while True:
-    intguess=int(input("Guess a number 1-10:"))
-    if  random_integer == intguess:
-        print("correct")
-        print(random_integer)
-        break
-    else: 
+    Guess=int(input("Guess Number"))
+    history.append (Guess)
+    if random_integer > Guess:
+        print("is less than")
+    else:
         print("wrong")
+        print(history)
+    if random_integer < Guess:
+        print("is greater than")
+    else:
+        print("wrong")
+        print(history)
+    if random_integer == Guess:
+        print("Correct!")
+        print(history)
+        break
 
-x=0    
-Guess=int(input(""))
+
 
 
